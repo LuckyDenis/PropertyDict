2079e257dd64612c332bf76d7995c898b1894bb7
Belavin Denis <belavin.d.andreevich@yandex.ru>
2020-02-04 16:49:03 +0300

    init project

--- PropertyDict/__about__.py
+++ PropertyDict/__about__.py
@@ -1,1 +1,33 @@
 # coding: utf8
+
+__all__ = ['EasterEgg', '__title__', '__author__',
+           '__email__', '__version__', '__url__',
+           '__description__', '__platforms__',
+           '__license__']
+
+
+class EasterEgg(object):
+    """
+╔══╗╔╗╔╦══╦╗╔╦═══╗
+╚╗╔╝║║║║╔╗║║║║╔══╝
+─║║─║╚╝║╚╝║║║║╚══╗
+─║║─║╔╗║╔╗║╚╝║╔══╝
+╔╝╚╗║║║║║║╠╗╔╣╚══╗
+╚══╝╚╝╚╩╝╚╝╚╝╚═══╝
+╔══╗╔═══╦═══╦╗─╔╗╔╗╔╦═══╦═══╦═══╗
+║╔╗║║╔══╣╔══╣╚═╝║║║║║╔══╣╔═╗║╔══╝
+║╚╝╚╣╚══╣╚══╣╔╗─║║╚╝║╚══╣╚═╝║╚══╗
+║╔═╗║╔══╣╔══╣║╚╗║║╔╗║╔══╣╔╗╔╣╔══╝
+║╚═╝║╚══╣╚══╣║─║║║║║║╚══╣║║║║╚══╗
+╚═══╩═══╩═══╩╝─╚╝╚╝╚╩═══╩╝╚╝╚═══╝
+    """
+
+
+__title__ = 'PropertyDict'
+__author__ = 'Belavin Denis'
+__email__ = 'belavin.d.andreevich@yandex.ru'
+__version__ = '0.0.0'
+__url__ = 'https://github.com/LuckyDenis/PropertyDict'
+__description__ = 'A simple way to travel through the dictionary.'
+__platforms__ = 'any'
+__license__ = 'Apache License 2.0'
